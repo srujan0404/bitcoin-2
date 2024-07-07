be988c2a96eb1e190c13a1d0a6ba15b747f1956c
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-07-08 00:26:14 +0530

    added functionalities for digest

--- solution/helpers/witnessTxId.js
+++ solution/helpers/witnessTxId.js
@@ -3,93 +3,75 @@ import { doubleSha256 } from "./Hashes.js";
 function witness_TxId(transaction) {
   let serialized = "";
   let witness = [];
-  let stack_items = 0;
-  let stack_items_witness = "";
-  // Serialize version (little-endian) must be 4 bytes
-  serialized += transaction.version
-    .toString(16)
-    .padStart(8, "0")
-    .match(/../g)
-    .reverse()
-    .join("");
 
-  serialized += "0001"; // marker + Flag
+  // Helper function to convert a number to little-endian hex and pad to specified length
+  const toLittleEndianHex = (num, length) => {
+    return num
+      .toString(16)
+      .padStart(length, "0")
+      .match(/../g)
+      .reverse()
+      .join("");
+  };
 
-  // Serialize number of inputs must be 1 byte
+  // Serialize version (4 bytes, little-endian)
+  serialized += toLittleEndianHex(transaction.version, 8);
+
+  // Serialize marker and flag (0001)
+  serialized += "0001";
+
+  // Serialize number of inputs (1 byte)
   serialized += transaction.vin.length.toString(16).padStart(2, "0");
+
   // Serialize inputs
   transaction.vin.forEach((input) => {
-    stack_items = 0; // setting up the stack items
-    stack_items_witness = ""; // setting up the stack items witness
+    let stackItemsWitness = "";
 
-    // Serialize txid (little-endian) must be 32 bytes
+    // Serialize txid (32 bytes, little-endian)
     serialized += input.txid.match(/../g).reverse().join("");
 
-    //setting up the witness field
-    stack_items = input.witness.length.toString(16).padStart(2, "0");
-    // Serialize signature length
-    input.witness.forEach((witness) => {
-      stack_items_witness += (witness.length / 2).toString(16).padStart(2, "0");
-      stack_items_witness += witness;
-    });
+    // Serialize vout (4 bytes, little-endian)
+    serialized += toLittleEndianHex(input.vout, 8);
 
-    witness.push(stack_items + stack_items_witness);
-
-    // Serialize vout (little-endian) must be 4 bytes
-    serialized += input.vout
-      .toString(16)
-      .padStart(8, "0")
-      .match(/../g)
-      .reverse()
-      .join("");
-
-    // Serialize scriptSig length
+    // Serialize scriptSig length and scriptSig
     serialized += (input.scriptsig.length / 2).toString(16).padStart(2, "0");
-    // Serialize scriptSig
     serialized += input.scriptsig;
 
-    // Serialize sequence (little-endian) - must be 4 bytes
-    serialized += input.sequence
-      .toString(16)
-      .padStart(8, "0")
-      .match(/../g)
-      .reverse()
-      .join("");
+    // Serialize sequence (4 bytes, little-endian)
+    serialized += toLittleEndianHex(input.sequence, 8);
+
+    // Serialize witness field
+    stackItemsWitness += input.witness.length.toString(16).padStart(2, "0");
+    input.witness.forEach((witness) => {
+      stackItemsWitness += (witness.length / 2).toString(16).padStart(2, "0");
+      stackItemsWitness += witness;
+    });
+
+    witness.push(stackItemsWitness);
   });
 
-  // Serialize number of outputs
+  // Serialize number of outputs (1 byte)
   serialized += transaction.vout.length.toString(16).padStart(2, "0");
 
   // Serialize outputs
   transaction.vout.forEach((output) => {
-    // Assuming 'output.value' is in Bitcoins, convert it to satoshis
-    const satoshis = output.value; // no need to conver to sathsis as they are already in sathosis
+    // Serialize the satoshis value (8 bytes, little-endian)
+    serialized += toLittleEndianHex(output.value, 16);
 
-    // Serialize the satoshis value
-    serialized += satoshis
-      .toString(16)
-      .padStart(16, "0")
-      .match(/../g)
-      .reverse()
-      .join("");
-
-    // Serialize scriptPubKey length
+    // Serialize scriptPubKey length and scriptPubKey
     serialized += (output.scriptpubkey.length / 2)
       .toString(16)
       .padStart(2, "0");
-    // Serialize scriptPubKey
     serialized += output.scriptpubkey;
   });
 
-  serialized += witness.join(""); // stack items
+  // Append witness data
+  serialized += witness.join("");
 
-  serialized += transaction.locktime
-    .toString(16)
-    .padStart(8, "0")
-    .match(/../g)
-    .reverse()
-    .join("");
+  // Serialize locktime (4 bytes, little-endian)
+  serialized += toLittleEndianHex(transaction.locktime, 8);
 
+  // Calculate txid
   const txid = doubleSha256(serialized).match(/../g).reverse().join("");
 
   return txid;
