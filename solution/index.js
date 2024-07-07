3a8937255c577ea63229d4a5d82683865f4e88ef
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-07-08 00:41:03 +0530

    changed the source

--- solution/index.js
+++ solution/index.js
@@ -1,177 +1,102 @@
 import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+import pkg from "elliptic";
+import { performance } from "perf_hooks";
+const { ec: EC } = pkg;
 import { ImpelmentCommands } from "./Helpers/ImplementCommands.js";
-import { SHA256, doubleSha256 } from "./Helpers/Hashes.js";
+import { SHA256, doubleSha256, OP_HASH160 } from "./Helpers/Hashes.js";
 import { serializeTxn } from "./Helpers/digests/serialize.js";
 import { messageDigestp2wpkh } from "./Helpers/digests/messageDigestp2wpkh.js";
-import { verifyECDSASignature } from "./Helpers/ecdsa.js";
-import { mine } from "./mine.js";
+import { messageDigest_p2sh } from "./Helpers/digests/messageDigest_p2sh.js";
 
 readAllFilesGetData("mempool");
+// readfilesFromFile();
+function readfilesFromFile() {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  // Replace 'all_p2pkh.txt' with the path to your text file
+  const filePath = path.join(__dirname, "all_p2pkh.txt");
+
+  // Read the file line by line
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading the file:", err);
+      return;
+    }
+
+    // Split the file content by new lines
+    const lines = data.split(/\r?\n/);
+
+    // Extract and print each filename
+    const inValidTransactions = [];
+    lines.forEach((line) => {
+      if (line) {
+        // Assuming the filename is the first word of the line
+        const filename = line.split(" ")[0];
+        const filePath = `mempool/${filename}.json`;
+        const fileContent = fs.readFileSync(filePath, "utf8");
+        const JsonData = JSON.parse(fileContent);
+        if (!verifyTransaction(JsonData)) {
+          inValidTransactions.push(getFileName(JsonData));
+        }
+        // console.log(inValidTransactions);
+      }
+    });
+    // write invalid transactions to the file
+    fs.writeFileSync(
+      "invalid_transactions.txt",
+      inValidTransactions.join("\n")
+    );
+  });
+}
 
 function readAllFilesGetData(FolderPath) {
   //read the valid transactions from the file if file not present keep it empty
-  const files = fs.readdirSync(FolderPath); // Reading all the files
-  let ValidData = [];
-  files.forEach((fileName) => {
-    const filePath = `${FolderPath}/${fileName}`;
-    const fileContent = fs.readFileSync(filePath, "utf8");
-    const JsonData = JSON.parse(fileContent);
-    const valid = isValidFileName(JsonData, fileName.slice(0, -5));
-    if (valid.types != undefined && valid.types.length == 1) {
-      valid.fileContent = JsonData;
-      valid.types = valid.types[0];
-      ValidData.push(valid);
-      // valid => { fileName, types } currently types has 1 element
-    }
+	@@ -114,8 +71,8 @@ function readAllFilesGetData(FolderPath) {
   });
 
-  let Data = [];
-  ValidData.forEach((e) => {
-    if (verifyTransaction(e.fileContent, e.types)) {
-      e.serializetx = serializeTxn(e.fileContent).filename;
-      Data.push(e);
-    }
-  });
+  // fs.writeFileSync("valid_transactions.txt", JSON.stringify(Data));
+  fs.writeFileSync(
+    "valid_transactions_Count.json",
+    JSON.stringify({
+      Data,
+    })
+	@@ -131,36 +88,6 @@ function readAllFilesGetData(FolderPath) {
+  console.log("v0_p2wsh", v0_p2wsh_valid, v0_p2wsht);
+  console.log("v1_p2tr", v1_p2tr_valid, v1_p2trt);
 
-  mine(Data);
+  let set = new Set();
 
-  return true;
-}
+  // ValidData.forEach((e) => {
+  //   // console.log(e.types[0]);
+  //   console.log(e.types);
+  // });
+  // now every file has been read and we have the valid data with its types
 
-function isValidFileName(JsonData, fileName) {
-  let serialized = serializeTxn(JsonData);
-  let types = Array.from(serialized.types);
-  let file = SHA256(
-    doubleSha256(serialized.filename).match(/../g).reverse().join("")
-  );
-  if (file == fileName) {
-    return { fileName, types };
-  }
-  return false;
-}
+  //write the fileNames to the
+  // let sets = new Set();
+  // // console.log(ValidData[0], "Valid Transactions");
+  // ValidData.forEach((e) => {
+  //   if (e.types.size != 1) {
+  //     if (!sets.has(e.types)) {
+  //       sets.add(e.types);
+  //     }
+  //   }
+  // });
 
-function verifyTransaction(transaction, type) {
-  // Assuming 'vin' is an array and we need to check every transaction
+  //write this to the file
+  // let setArray = Array.from(sets); // Assuming 'sets' is an iterable of Set objects
+  // for (let i = 0; i < setArray.length; i++) {
+  //   // Convert each Set to an Array to use the join method
+  //   setArray[i] = Array.from(setArray[i]).join(" ");
+  // }
 
-  if (type == "v0_p2wpkh") {
-    let valid = true;
-    transaction.vin.forEach((e, index) => {
-      const message = messageDigestp2wpkh(transaction, index);
-      const isValid = verifyECDSASignature(e.witness[1], e.witness[0], message);
-      if (!isValid) {
-        valid = false;
-      }
-    });
+  // fs.writeFileSync("set.txt", setArray.join("\n"));
 
-    return valid;
-  } else if (type == "p2sh" && transaction.vin[0].witness == undefined) {
-    let validTransaction = true;
-    transaction.vin.forEach((e, index) => {
-      //combine the signature scrpit and pubkey script
-      let commands = "";
-      commands = e.scriptsig_asm + " " + e.prevout.scriptpubkey_asm;
-      commands = commands.split(" ");
-      // console.log(commands);
-      let stack = [];
-      stack = ImpelmentCommands(stack, commands, type, transaction, index);
-      if (stack.pop() == false) validTransaction = false;
-      commands = e.inner_redeemscript_asm.split(" ");
-      stack = ImpelmentCommands(stack, commands, type, transaction, index);
-      if (validTransaction && stack[0] == true && stack.length == 1);
-      else {
-        validTransaction = false;
-      }
-    });
-    return validTransaction;
-  } else if (type == "p2pkh") {
-    let stack = [];
-    let result = true;
-    transaction.vin.forEach((e, index) => {
-      stack = [];
-      let commands = "";
-      commands = e.scriptsig_asm + " " + e.prevout.scriptpubkey_asm;
-      commands = commands.split(" ");
-      stack = ImpelmentCommands(stack, commands, type, transaction, index);
-      if (stack[0] == true && stack.length == 1);
-      else {
-        result = false;
-      }
-    });
-    if (result == false) return false;
-    if (stack[0] == true && stack.length == 1) return true;
-    return false;
-  } else if (type == "v0_p2wsh") {
-    let valid = true;
-    transaction.vin.forEach((e, index) => {
-      const messagehash = messageDigestp2wpkh(transaction, index, "p2wsh");
-      const getsignatures = e.inner_witnessscript_asm
-        .split(" ")[0]
-        .split("_")[2];
-      let stack = [];
-      for (let i = 0; i < getsignatures; i++) {
-        stack.push(e.witness[i + 1]);
-      }
-      let commands = e.inner_witnessscript_asm.split(" ");
-      commands.forEach((command) => {
-        if (command.startsWith("OP_PUSHBYTES_") || command == "OP_0") {
-          // Just Leave them as the next bytes will be auto matically pushed
-        } else if (command === "OP_EQUAL" || command === "OP_EQUALVERIFY") {
-          let stackElement1 = stack.pop();
-          let stackElement2 = stack.pop();
-          stack.push(stackElement1 === stackElement2);
-        } else if (command == "OP_DUP") {
-          let stackElement = stack.pop();
-          stack.push(stackElement);
-          stack.push(stackElement);
-        } else if (command.startsWith("OP_PUSHNUM")) {
-          stack.push(parseInt(command.split("_")[2]));
-        } else if (command == "OP_CHECKMULTISIG") {
-          let noOfKeys = stack.pop();
-          let keys = [];
-          for (let i = 0; i < noOfKeys; i++) {
-            keys.push(stack.pop());
-          }
-          let noOfSignatures = stack.pop();
-          let signatures = [];
-          for (let i = 0; i < noOfSignatures; i++) {
-            signatures.push(stack.pop());
-          }
-          //now we have the keys and signatures
-          //check if the signatures are valid with trial of all the public keys
-          for (let i = 0; i < noOfSignatures; i++) {
-            let isValid = false;
-            for (let j = 0; j < noOfKeys; j++) {
-              isValid = verifyECDSASignature(
-                keys[j],
-                signatures[i],
-                messagehash
-              );
-              if (isValid) break;
-            }
-            if (!isValid) {
-              valid = false;
-            }
-          }
-          if (valid == false) return false;
-          stack.push(true);
-          // console.log(stack);
-        } else {
-          stack.push(command); // Push other commands directly onto the stack
-        }
-      });
-    });
-
-    return valid;
-  } else if (type == "p2sh") {
-    if (transaction.vin[0].witness.length == 2) {
-      type = "p2wpkh";
-      const message = messageDigestp2wpkh(transaction, 0, "p2sh_p2wpkh");
-      const signature = transaction.vin[0].witness[0];
-      const pubkey = transaction.vin[0].witness[1];
-      const valid = verifyECDSASignature(pubkey, signature, message);
-      return valid;
-    }
-    // console.log(transaction.vin[0].witness.length);
-  }
-  return false;
+  // fs.writeFileSync("_transactions.txt", ValidData);
+  // Return the valid data
+  return ValidData;
 }
--- solution/mine.js
+++ solution/mine.js
@@ -1,17 +1,16 @@
-import fs from "fs";
-import { createBlock } from "./Helpers/Block/createBlock.js";
-import { merkle_root } from "./Helpers/Block/merkleroot.js";
-import { doubleSha256 } from "./Helpers/Hashes.js";
-import { serializeTxn } from "./Helpers/digests/serialize.js";
-import { coinBase } from "./Helpers/Block/coinBase.js";
 import { calculateWeight } from "./Helpers/Block/calculateWeight.js";
 import { witness_TxId } from "./Helpers/witnessTxId.js";
 
-function mine(data) {
-  let validTransactions = [];
-  let txids = [];
-  for (const transaction of data) {
+const validData = fs.readFileSync("valid_transactions_Count.json", "utf8");
+const data = JSON.parse(validData);
+console.log("Start Mining");
+let validTransactions = [];
+let txids = [];
+let i = 0;
+for (const key in data) {
+  for (const transaction of data[key]) {
     const { fileName, types, fileContent, serializetx } = transaction;
+    //Extracted the files that the valid and verification done
 
     const serialize = serializeTxn(fileContent);
     const txid = doubleSha256(serialize.filename)
@@ -20,68 +19,74 @@ function mine(data) {
       .join("");
     const data = fs.readFileSync("mempool/" + fileName + ".json", "utf8");
     const txn = JSON.parse(data);
-
-    validTransactions.push(txn);
-    txids.push(txid);
+    // console.log(txn);
+    validTransactions[i] = txn;
+    txids[i] = txid;
+    i++;
   }
-
-  let max_weight = 4 * 1000 * 1000;
-  let current_weight = 320;
-  let transactions = [];
-  let witnessTxs = [];
-  for (let i = 0; i < validTransactions.length; i++) {
-    const { complete_weight: tx_wt, tx_type } = calculateWeight(
-      validTransactions[i]
-    );
-    if (tx_type === undefined) continue;
-    if (tx_type === "SEGWIT") {
-      witnessTxs.push(witness_TxId(validTransactions[i]));
-    } else {
-      witnessTxs.push(txids[i]);
-    }
-    if (tx_wt) {
-      if (current_weight + tx_wt <= max_weight) {
-        transactions.push(txids[i]);
-        current_weight += tx_wt;
-      } else {
-        break;
-      }
-    }
-  }
-  // add the witness reserved value in the answer
-  witnessTxs.unshift((0).toString(16).padStart(64, "0"));
-  let coinbaseTransacton = coinBase(witnessTxs);
-  const coinBaseTxId = doubleSha256(coinbaseTransacton)
-    .match(/../g)
-    .reverse()
-    .join("");
-
-  transactions.unshift(coinBaseTxId);
-  const merkleRoot = merkle_root(transactions);
-  let block = createBlock(merkleRoot, 0); //Intially Nonce is Zero
-
-  //write an output file
-  // name: output.txt
-  // 1 Line -> Block Header
-  // 2 Line -> Coinbase Transaction
-  // 3 Line -> No of transaction ids
-  let nonce = 0;
-  const dificulty = Buffer.from(
-    "0000ffff00000000000000000000000000000000000000000000000000000000",
-    "hex"
-  );
-  let blockHash = doubleSha256(block).match(/../g).reverse().join("");
-  while (dificulty.compare(Buffer.from(blockHash, "hex")) < 0) {
-    nonce++;
-    block = createBlock(merkleRoot, nonce);
-    blockHash = doubleSha256(block).match(/../g).reverse().join("");
-  }
-
-  const AllTransactioinIdsJoined = transactions.join("\n");
-  fs.writeFileSync(
-    "output.txt",
-    block + "\n" + coinbaseTransacton + "\n" + AllTransactioinIdsJoined
-  );
 }
 
-export { mine };
+let max_weight = 4 * 1000 * 1000 - 320;
+let current_weight = 0;
+let transactions = [];
+let witnessTxs = [];
+var z = 3;
+for (let i = 0; i < validTransactions.length; i++) {
+  // console.log(validTransactions[i]);
+  const { complete_weight, tx_type } = calculateWeight(validTransactions[i]);
+  if (tx_type === undefined) continue;
+  if (tx_type === "SEGWIT") {
+    witnessTxs.push(witness_TxId(validTransactions[i]));
+    if (z != 0) {
+      console.log(witness_TxId(validTransactions[i]), " ", txids[i]);
+      z--;
+    }
+  } else {
+    witnessTxs.push(txids[i]);
+  }
+  if (complete_weight) {
+    if (current_weight + complete_weight <= max_weight) {
+      transactions.push(txids[i]);
+      current_weight += complete_weight;
+    } else {
+      break;
+    }
+  }
+}
+let nonce = 0;
+// add the witness reserved value in the answer
+witnessTxs.unshift((0).toString(16).padStart(64, "0"));
+fs.writeFileSync("witnessTxs.json", JSON.stringify(witnessTxs));
+let coinbaseTransacton = coinBase(witnessTxs);
+const coinBaseTxId = doubleSha256(coinbaseTransacton)
+  .match(/../g)
+  .reverse()
+  .join("");
+
+// console.log(transactions);
+// const merkleRoot = merkle_root(txids);
+transactions.unshift(coinBaseTxId);
+const merkleRoot = merkle_root(transactions);
+let block = createBlock(merkleRoot, nonce);
+
+//write an output file
+// name: output.txt
+// 1 Line -> Block Header
+// 2 Line -> Coinbase Transaction
+// 3 Line -> No of transaction ids
+const txidsa = transactions.join("\n");
+const dificulty = Buffer.from(
+  "0000ffff00000000000000000000000000000000000000000000000000000000",
+  "hex"
+);
+let blockHash = doubleSha256(block).match(/../g).reverse().join("");
+while (dificulty.compare(Buffer.from(blockHash, "hex")) < 0) {
+  nonce++;
+  block = createBlock(merkleRoot, nonce);
+  blockHash = doubleSha256(block).match(/../g).reverse().join("");
+}
+console.log("Done");
+fs.writeFileSync(
+  "output.txt",
+  block + "\n" + coinbaseTransacton + "\n" + txidsa
+);
